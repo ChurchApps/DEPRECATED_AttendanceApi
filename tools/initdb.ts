d95968d9d3c618bb2d444afb24e0845a6dd8ee0e
Jeremy Zongker <jeremy@trilitech.com>
2021-11-10 16:45:04 -0600

    Moved config to json

--- tools/initdb.ts
+++ tools/initdb.ts
@@ -1,37 +1,39 @@
 import dotenv from "dotenv";
+import { Environment } from "../src/helpers/Environment";
 import { Pool } from "../src/apiBase/pool";
 import { DBCreator } from "../src/apiBase/tools/DBCreator"
 
 const init = async () => {
-    dotenv.config();
-    console.log("Connecting");
-    Pool.initPool();
+  dotenv.config();
+  Environment.init(process.env.APP_ENV);
+  console.log("Connecting");
+  Pool.initPool();
 
-    const attendanceTables: { title: string, file: string }[] = [
-        { title: "Campuses", file: "campuses.mysql" },
-        { title: "Services", file: "services.mysql" },
-        { title: "Service Times", file: "serviceTimes.mysql" },
-        { title: "Group Service Times", file: "groupServiceTimes.mysql" },
-        { title: "Sessions", file: "sessions.mysql" },
-        { title: "Visits", file: "visits.mysql" },
-        { title: "Visit Sessions", file: "visitSessions.mysql" }
-    ];
+  const attendanceTables: { title: string, file: string }[] = [
+    { title: "Campuses", file: "campuses.mysql" },
+    { title: "Services", file: "services.mysql" },
+    { title: "Service Times", file: "serviceTimes.mysql" },
+    { title: "Group Service Times", file: "groupServiceTimes.mysql" },
+    { title: "Sessions", file: "sessions.mysql" },
+    { title: "Visits", file: "visits.mysql" },
+    { title: "Visit Sessions", file: "visitSessions.mysql" }
+  ];
 
-    await DBCreator.init(["Settings"]);
-    await initTables("Attendance", attendanceTables);
+  await DBCreator.init(["Settings"]);
+  await initTables("Attendance", attendanceTables);
 }
 
 const initTables = async (displayName: string, tables: { title: string, file: string }[]) => {
-    console.log("");
-    console.log("SECTION: " + displayName);
-    for (const table of tables) await DBCreator.runScript(table.title, "./tools/dbScripts/" + table.file, false);
+  console.log("");
+  console.log("SECTION: " + displayName);
+  for (const table of tables) await DBCreator.runScript(table.title, "./tools/dbScripts/" + table.file, false);
 }
 
 init()
-    .then(() => { console.log("Database Created"); process.exit(0); })
-    .catch((ex) => {
-        console.log(ex);
-        console.log("Database not created due to errors");
-        process.exit(0);
-    });
+  .then(() => { console.log("Database Created"); process.exit(0); })
+  .catch((ex) => {
+    console.log(ex);
+    console.log("Database not created due to errors");
+    process.exit(0);
+  });
 
