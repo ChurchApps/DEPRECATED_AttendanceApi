b9cf90417fec76d7f135c176f9d8178408d9b273
Jeremy Zongker <jeremy@zongker.net>
2025-07-02 00:52:10 -0500

    Unified lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,19 +8,37 @@ module.exports = [
       parser: tsParser,
       parserOptions: {
         project: './tsconfig.json',
-        tsconfigRootDir: __dirname
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2020,
+        sourceType: 'module'
       }
     },
     plugins: {
       '@typescript-eslint': tsPlugin
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
+      // TypeScript-specific rules
+      '@typescript-eslint/no-unused-vars': [
+        'warn', 
+        { 
+          argsIgnorePattern: '^_|^req$|^res$|^au$|^ex$|^e$|^bind$',
+          varsIgnorePattern: '^_|^start$|^result$|^app$',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      
+      // General rules
       'no-console': 'error',
-      'prefer-const': 'error'
+      'prefer-const': 'error',
+      
+      // Code style (enforced by Prettier, but useful for linting)
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'double'],
+      'comma-dangle': ['error', 'never']
     }
   }
 ];
